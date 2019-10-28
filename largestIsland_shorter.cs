c7d533744d04476491ee3085e64c4035bcc9b4b6
Lê Văn Đôn <mrledon90@hotmail.com>
2019-10-28 19:27:38 +0700

    add Shorter of version 2

--- largestIsland_shorter.cs
+++ largestIsland_shorter.cs
@@ -1,8 +1,8 @@
 int largestIsland(int[][] map)
 {
     int C = map.Length;
-    HashSet<int> oldSearch = new HashSet<int>();
-    int maxvalue = 0;
+    HashSet<int> oSe = new HashSet<int>();
+    int mvl = 0;
     int id = 0;
     for (int i = 0; i < C; i++)
     {
@@ -10,42 +10,42 @@ int largestIsland(int[][] map)
         for (int j = 0; j < D; j++)
         {
             id=id+1;
-            if (map[i][j] == 1 && !oldSearch.Contains(id))
+            if (map[i][j] == 1 && !oSe.Contains(id))
             {
-                int oldCount = oldSearch.Count;
-                SearchItem(oldSearch, map, id, i, j, D, C);
-                int newsCount = oldSearch.Count;
-                int numberElement = newsCount - oldCount;
-                maxvalue = numberElement > maxvalue ? numberElement : maxvalue;
+                int oldCount = oSe.Count;
+                SI(oSe, map, id, i, j, D, C);
+                int newsCount = oSe.Count;
+                int nel = newsCount - oldCount;
+                mvl = nel > mvl ? nel : mvl;
             }
         }
     }
-    return maxvalue;
+    return mvl;
 }
 
-void SearchItem(HashSet<int> oldSearch, int[][] map,int id, int i, int j, int D, int C)
+void SI(HashSet<int> oSe, int[][] map,int id, int i, int j, int D, int C)
 {
-    if(j<= D-1 && j >= 0 && i >= 0 && i <= C -1  && !oldSearch.Contains(id))
+    if(j<= D-1 && j >= 0 && i >= 0 && i <= C -1  && !oSe.Contains(id))
     {
         if (map[i][j] == 1)
         {
-            oldSearch.Add(id);
-            SearchItem(oldSearch, map, id - 1, i, j - 1, D, C);
-            SearchItem(oldSearch, map, id + 1, i, j + 1, D, C);
+            oSe.Add(id);
+            SI(oSe, map, id - 1, i, j - 1, D, C);
+            SI(oSe, map, id + 1, i, j + 1, D, C);
 
             if (i > 0 && map[i-1] != null)
             {
                 int D1 = map[i - 1].Length;
-                SearchItem(oldSearch, map, id - D1 - 1, i - 1, j - 1, D1, C);
-                SearchItem(oldSearch, map, id - D1, i - 1, j, D1, C);
-                SearchItem(oldSearch, map, id - D1 + 1, i - 1, j + 1, D1, C);
+                SI(oSe, map, id - D1 - 1, i - 1, j - 1, D1, C);
+                SI(oSe, map, id - D1, i - 1, j, D1, C);
+                SI(oSe, map, id - D1 + 1, i - 1, j + 1, D1, C);
             }
             if (i < C - 1 && map[i+1] != null)
             {
                 int D2 = map[i + 1].Length;
-                SearchItem(oldSearch, map, id + D - 1, i + 1, j - 1, D2, C);
-                SearchItem(oldSearch, map, id + D, i + 1, j, D2, C);
-                SearchItem(oldSearch, map, id + D + 1, i + 1, j + 1, D2, C);
+                SI(oSe, map, id + D - 1, i + 1, j - 1, D2, C);
+                SI(oSe, map, id + D, i + 1, j, D2, C);
+                SI(oSe, map, id + D + 1, i + 1, j + 1, D2, C);
             }
         }
     }   
